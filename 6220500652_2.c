03f7583e147ed2932e6d437cfc02e47cbe0a59a0
b6220500652 <narisara.kaa@ku.th>
2021-03-29 19:56:13 +0700

    uncomplete

--- 6220500652_2.c
+++ 6220500652_2.c
@@ -1,10 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int n,i,a;
+	int n,i,a[n];
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&a);
 	}
+	for(i=0;i<n;i++)
+	{
+		printf("*\n");
+			if(a[i]!=a[i+1])
+			{
+				continue;
+			}
+			else if(a[i]==a[i+1])
+			{
+				printf("%d",a[i]);
+			}	
+		
+	}
+	
 }
+
---
+++ 6220500652_3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,t,a[n][n],b[m][m],c[t][t],i,j;
+	scanf("%d %d %d",&n,&m,&t);
+	
+}
